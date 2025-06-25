a2e7fab46a668b46a5d21852508b1b73267812c0
shakil <kazimuhammadullah@gmail.com>
2025-06-25 17:07:54 +0600

    date field

--- app/Http/Controllers/Api/FrontendController.php
+++ app/Http/Controllers/Api/FrontendController.php
@@ -579,7 +579,7 @@ class FrontendController extends Controller
 
         $openingDayTime = [
             'sunday_to_friday' => [
-                'day' => $company->opening_time ?? null,
+                'day' => "Saturday to Friday" ?? null,
                 'opening_time' => $company->opening_time ?? null,
                 'closing_time' => $company->closing_time ?? null,
             ],
